1e7c55e6bafc6f63fdc06375fa9faf8d39c2ab35
Navraj Narula <navrajnarula@gmail.com>
2018-01-30 14:26:19 -0500

    add text for context

--- hw/A1/sketch_clock.js
+++ hw/A1/sketch_clock.js
@@ -21,19 +21,26 @@ function draw() {
   ellipse(350, 80, map(hour(), 0, 59, 10, 150), map(hour(), 0, 59, 10, 150));
   
   // cone color
-  fill('Wheat');
 
+  fill('Wheat');
   triangle(240, 205, 460, 205, 350, 460);
 
   // text color
-  fill('Black');
+  fill("#4d2600");
 
+  push(); // start new drawing state
   // translate text according to seconds
   translate(10, map(second(), 0, 59, 10, 150));
-
-  // print text
+  // print time
   textSize(20);
   text("Now " + hour() + ":" + minute() + ":" + second(), 10, 150);
+  pop(); // end drawing state
+
+  // print info
+  textSize(10);
+  text("chocolate cherry grows by hour", 500, 530);
+  text("ice cream curves by minute (above/under cone)",500, 540);
+  text("time text translates by seconds", 500, 550);
 
 }
 
