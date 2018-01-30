9f380c4fc93988916ce0cafbb4b9de6777e0ce1c
Navraj Narula <navrajnarula@gmail.com>
2018-01-30 13:48:22 -0500

    add mapping for sec,min,hour and translation

--- hw/A1/sketch_clock.js
+++ hw/A1/sketch_clock.js
@@ -1,24 +1,40 @@
 function setup() {
   createCanvas(720, 560);
-
 }
 
 function draw() {
+
+  // canvas color
 	background('AntiqueWhite');
 
+  // ice cream color
 	fill('MistyRose');
 
-  	//arc(350, 200, 200, 225, -PI, 0);
-  	arc(350, 200, 200, 225, -PI, 0);
+  // ice cream shape
+  // arc shifts according to seconds
+  arc(350, 200, 200, 225, map(minute(), 59, 0, 30, 60), 0);
 
-  	fill('#4d2600');
+  // chocolate cherry color
+  fill('#4d2600');
 
-    //ellipse(310, 200, 5, map(second(), 59, 0, 0, 59));
-    //ellipse(310, 100, 5, map(second(), 0, 59, 300, 5));
+  // chocolate cherry grows according to hours
+  ellipse(350, 80, map(hour(), 0, 59, 10, 150), map(hour(), 0, 59, 10, 150));
+  
+  // cone color
+  fill('Wheat');
 
-  	fill('Wheat');
-  	triangle(350-200 / 2, 200 + 5, 350 + 200 / 2, 200 + 5, 350, 200 + 200 * 1.3);
+  triangle(240, 205, 460, 205, 350, 460);
+
+  // text color
+  fill('Black');
+
+  // translate text according to seconds
+  translate(10, map(second(), 0, 59, 10, 150));
+
+  // print text
+  textSize(20);
+  text("Now " + hour() + ":" + minute() + ":" + second(), 10, 150);
 
 }
 
-// reference aid: http://alpha.editor.p5js.org/ellacyt/sketches/ByaJx92j-
+// reference aid (for cone): http://alpha.editor.p5js.org/ellacyt/sketches/ByaJx92j-
